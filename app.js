11ba0d87ef9229cfce92680829b44949a6c7255d
Om Nepal <nepalom15@gmail.com>
2025-04-08 00:19:16 -0500

    feat: add new product feature on admin mode and use multer

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ const checkAuthStatusMiddleware = require('./middlewares/check-auth')
 const authRoutes = require('./routes/auth.routes')
 const productRoutes = require('./routes/products.routes')
 const baseRoutes = require('./routes/base.routes')
+const adminRoutes = require('./routes/admin.routes')
 
 const app = express()
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
 
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false }))
@@ -33,6 +35,7 @@ app.use(addCsrfTokenMiddleware)//just makes the token available for views using 
 app.use(baseRoutes)
 app.use(authRoutes)
 app.use(productRoutes)
+app.use('/admin', adminRoutes) //only path that start with /admin will make it to the admin routes. so we don't have to add /admin to every admin routes
 
 app.use(errorHandlerMiddleware)
 
