10113d435d8e4767556e16913b6985fcac6f6c23
Om Nepal <nepalom15@gmail.com>
2025-04-11 18:11:52 -0500

    feat: add all products display and view details feature on the customer side

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,11 +1,13 @@
 const express = require('express')
 
+const productsController = require('../controllers/products.controller')
+
 const authController = require('../controllers/auth.controller')
 
 const router = express.Router()
 
-router.get('/products', function (req, res) {
-  res.render('customer/products/all-products')
-})
+router.get('/products', productsController.getAllProducts)
+
+router.get('/products/:id', productsController.getProductDetails);
 
 module.exports = router
